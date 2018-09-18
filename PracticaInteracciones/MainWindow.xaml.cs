00b1906f5eb985c80720f3f277948d482b8a52e0
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-18 08:51:01 -0700

    Agregar archivos de proyecto.

---
+++ PracticaInteracciones/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaInteracciones
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtcuenta.Text = "Texto Modifiado";
+        }
+
+        private void sldporcenprop_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblporcenprop.Text =
+                sldporcenprop.Value.ToString();
+        }
+    }
+}
